8e52fcff2d02a648b16f5d65e9d7236e32e27d12
@mhyasaei <mhyasaei@uwaterloo.ca>
2023-08-25 14:07:27 -0400

    sortin garray using insertion algorithm

---
+++ insertionSort.js
@@ -0,0 +1,18 @@
+const insertionSort = (arr) => {
+    let temp, targetValue
+    for(let i = 1; i < arr.length; i++){
+        let prevIndex = i - 1
+        targetValue = arr[i]
+        while(prevIndex >= 0){ 
+            if(arr[prevIndex] > targetValue){
+                temp = arr[prevIndex + 1]
+                arr[prevIndex + 1] = arr[prevIndex]
+                arr[prevIndex] = temp
+            }
+            prevIndex--
+        }
+    }
+    return arr
+}
+
+console.log(insertionSort([5,4,3,2,1]))
